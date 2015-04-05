0e3fac79c07a81dca2799d1b794b4e0d1fd62c5a
Richard Wong <richard@inkling.com>
2015-04-04 21:38:13 -0700

    Create login form.

--- tfa/views.py
+++ tfa/views.py
@@ -12,6 +12,10 @@ class RegisterForm(forms.Form):
 	username = forms.CharField(label='Username', max_length=30)
 	password = forms.CharField(label='Password', max_length=100, widget=forms.PasswordInput())
 
+class LoginForm(forms.Form):
+	username = forms.CharField(label='Username', max_length=30)
+	password = forms.CharField(label='Password', max_length=100, widget=forms.PasswordInput())
+	tfaToken = forms.CharField(label='TFA Token', max_length=6)
 # Create your views here.
 
 def index(request):
@@ -32,6 +36,7 @@ def register(request):
 			uri = seed.provisioning_uri(username)
 			secret = urlparse.parse_qs(urlparse.urlparse(uri).query)['secret'][0]
 
+			# This can fail if username is not unique. TODO: wrap in try
 			newUser = User(username=username, password=password, tfaEnabled=True, tfaSecret=secret)
 			newUser.save()
 
@@ -43,8 +48,7 @@ def register(request):
 		else:
 			return render(request, 'tfa/register.html', {'form': RegisterForm()})
 	else:
-		form = RegisterForm()
-		return render(request, 'tfa/register.html', {'form': form})
+		return render(request, 'tfa/register.html', {'form': RegisterForm()})
 
 def tfaSetup(request):
 	return render(request, 'tfa/tfaSetup.html', {
@@ -53,7 +57,36 @@ def tfaSetup(request):
 	})
 
 def login(request):
-	return HttpResponse("You can log in here.")
+	if request.method == 'POST':
+		form = LoginForm(request.POST)
 
-def landing(request, request_id):
-	return HttpResponse("You have successfully logged in!")
+		if form.is_valid():
+			user = User.objects.filter(
+				username__exact=form.cleaned_data['username']
+			).get()
+
+			print(user)
+
+			if not user:
+				return HttpResponse("No such user!")
+
+			if user.password != form.cleaned_data['password']:
+				return HttpResponse("Bad password!")
+
+			expectedToken = "%06d" % pyotp.TOTP(user.tfaSecret).now()
+			if form.cleaned_data['tfaToken'] != str(expectedToken):
+				return HttpResponse("Bad TFA token! Expected " + str(expectedToken) + ", got " + form.cleaned_data['tfaToken'])
+
+			return redirect(reverse('landing', kwargs={'user_id': user.id}))
+		else:
+			return render(request, 'tfa/login.html', {'form': LoginForm()})
+	else:
+		return render(request, 'tfa/login.html', {'form': LoginForm()})
+
+def landing(request, **kwargs):
+	user = User.objects.filter(
+		id__exact=kwargs.get('user_id')
+	).get()
+	return render(request, 'tfa/landing.html', {'username': user.username})
+
+
